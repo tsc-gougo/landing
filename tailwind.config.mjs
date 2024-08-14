ccb8592f62f06e648e081837d8e220cbd914e410
Luis Palma <lpalma@tsc.com.gt>
2024-08-14 07:46:47 -0600

    Cover (Top)

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,7 +7,26 @@ export default {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      poppins:
+        'Poppins, ui-sans-serif, system-ui, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+    },
+    extend: {
+      colors: {
+        orange: {
+          50: "#fbe9e7",
+          100: "#feccbc",
+          200: "#feac90",
+          300: "#fe8b64",
+          400: "#fd7241",
+          500: "#fd5a1f",
+          600: "#f2541b",
+          700: "#e44d16",
+          800: "#d64612",
+          900: "#bd3909",
+        },
+      },
+    },
   },
   darkMode: "class",
   plugins: [nextui()],
